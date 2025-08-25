145e5658d05886de3bbd3758e49c089f62ff6096
Harshit Bhardwaj <harshit704892@gmail.com>
2025-08-25 12:27:07 +0530

    Initial commit with project files

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+ 
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='main'>
+        <div className="read">
+          read
+        </div>
+
+        <div className="create">
+    create
+        </div>
+        
+
+      </div>
+    </>
+  )
+}
+
+export default App
