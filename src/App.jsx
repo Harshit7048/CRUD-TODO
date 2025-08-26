6966621ff5865d5b7de7d7810be8448c43b957de
Harshit Bhardwaj <harshit704892@gmail.com>
2025-08-26 23:37:39 +0530

    daily update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import { useState } from 'react'
- 
+
 import './App.css'
 
 function App() {
+
+  const [newTask, setNewTask] = useState({
+    taskTitle:"",
+    taskDate: "",
+    taskTime:""
+  })
+
+  const [allTask, setAllTask] = useState([])
+
+  const addTask = (e) => {
+    e.preventDefault();
+    console.log("I am adding task");
+    let form = document.getElementById('add-task');
+
+    console.log(form.taskTitle.value, form.taskDate.value, form.taskTime.value);
+
+    let newKey = `TSK-${Math.floor(Math.random() * 900) + 100}`
  
+    setNewTask({ taskTitle: `${form.taskTitle.value}`, takDate: form.taskDate.value, taskTime: form.taskTime.value })
+
+    console.log(newTask);
+  }
+
 
   return (
     <>
@@ -13,17 +35,17 @@ function App() {
         </div>
 
         <div className="create">
-        <div className="add-box">
-           <h2>Add A Task</h2>
-          <form className='inputs' id='add-task'>
-            <input type="text" placeholder='Title' name='task-title'/>
-            <input type="date" id='date' value="0" name='task-date'/>
-            <input type="time" id='time' value="0" name='task-time'/>
-          </form>
-          <button id='add-task-btn'>Add</button>
+          <div className="add-box">
+            <h2>Add A Task</h2>
+            <form className='inputs' id='add-task'>
+              <input type="text" placeholder='Title' name='taskTitle' />
+              <input type="date" id='date' name='taskDate' />
+              <input type="time" id='time' name='taskTime' />
+            </form>
+            <button id='add-task-btn' onClick={addTask}>Add</button>
+          </div>
         </div>
-        </div>
-        
+
 
       </div>
     </>
